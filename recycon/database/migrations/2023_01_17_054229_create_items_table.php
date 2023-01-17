be6955b4ba39b12248838af7578902474c364f1d
ReySnts <76163718+ReySnts@users.noreply.github.com>
2023-01-17 20:22:41 +0700

    Migration

---
+++ recycon/database/migrations/2023_01_17_054229_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false)->unique();
+            $table->integer('price')->nullable(false);
+            $table->string('description')->nullable(false);
+            $table->string('image')->nullable(false);
+            $table->string('category')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
